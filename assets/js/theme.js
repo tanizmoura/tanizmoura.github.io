7169f95ff8c556d890c1b30714aa9565f1d472fe
tanizmoura <tanise.mourax3@hotmail.com>
2025-08-30 00:34:19 -0300

    salvando e atualizando tema com localStorage

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,32 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement;
 
-function changeTheme(){
-    const currentTheme = rootHtml.getAttribute('data-theme');
+if(localStorage.getItem('theme')){
+    rootHtml.setAttribute('data-theme', localStorage.getItem('theme'));
+    
+    if(localStorage.getItem('theme') === 'dark'){
+        toggleTheme.classList.add("bi-sun");
+        toggleTheme.classList.remove("bi-moon-stars");
+    }else{
+        toggleTheme.classList.add("bi-moon-stars");
+        toggleTheme.classList.remove("bi-sun");
+    }
+}
 
-    currentTheme === 'dark'? rootHtml.setAttribute('data-theme', 'light') : rootHtml.setAttribute('data-theme', 'dark');
+function changeTheme(){
+    const currentTheme = localStorage.getItem('theme');
+
+    currentTheme === 'dark'? localStorage.setItem('theme', 'light') & rootHtml.setAttribute('data-theme', 'light'): localStorage.setItem('theme', 'dark') & rootHtml.setAttribute('data-theme', 'dark');
 
     toggleTheme.classList.toggle("bi-moon-stars");
     toggleTheme.classList.toggle("bi-sun");
+
+    console.log(localStorage.getItem('theme'));
+
 }
 
+
 toggleTheme.addEventListener("click", changeTheme);
+
+
+
